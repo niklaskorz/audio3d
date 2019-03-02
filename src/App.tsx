1ba8840371a09c9cc8e44589d6c851a106900c22
Niklas Korz <korz.niklask@gmail.com>
2019-03-02 17:13:30 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,26 @@
 import React, { Component } from "react";
-import { RoomDimensions, RoomMaterials } from "resonance-audio";
-import Scene from "./react-resonance/Scene";
-import Source from "./react-resonance/Source";
+import AudioDemo from "./AudioDemo";
 import "./App.css";
-import { Vector3 } from "./react-resonance/utils";
-
-const dimensions: RoomDimensions = {
-  width: 5,
-  height: 5,
-  depth: 5
-};
-
-const materials: RoomMaterials = {
-  left: "brick-bare",
-  right: "curtain-heavy",
-  front: "marble",
-  back: "glass-thin",
-  down: "grass",
-  up: "transparent"
-};
-
-/**
- * f(0s) = (0, 1)
- * f(1s) = (1, 0)
- * f(2s) = (0, -1)
- * f(3s) = (-1, 0)
- * f(4s) = (0, 1)
- * f(t) = (sin(t / 4s * 2 * pi), cos(t / 4s * 2 * pi))
- */
-
-const f = (t: number): Vector3 => [
-  3 * Math.sin((t / 4000) * 2 * Math.PI),
-  0,
-  3 * Math.cos((t / 4000) * 2 * Math.PI)
-];
 
 interface State {
-  position: Vector3;
   isPlaying: boolean;
 }
 
 class App extends Component<{}, State> {
   state: State = {
-    position: [0, 0, 1],
     isPlaying: false
   };
-  raf!: number;
-  lastT: number = 0;
+  demo = new AudioDemo();
 
-  /*
-  componentDidMount(): void {
-    this.raf = requestAnimationFrame(this.updatePosition);
-  }
-
-  componentWillUnmount(): void {
-    cancelAnimationFrame(this.raf);
-  }
-
-  updatePosition: FrameRequestCallback = t => {
-    if (t - this.lastT >= 100) {
-      this.lastT = t;
-      this.setState({ position: f(t) });
+  togglePlayback = () => {
+    if (!this.state.isPlaying) {
+      this.setState({ isPlaying: true });
+      this.demo.start("/audio/breakbeat.wav");
+    } else {
+      this.setState({ isPlaying: false });
+      this.demo.stop();
     }
-    this.raf = requestAnimationFrame(this.updatePosition);
   };
-  */
 
   render() {
     return (
@@ -73,50 +29,10 @@ class App extends Component<{}, State> {
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
-          <button
-            onClick={() =>
-              this.setState(state => ({
-                isPlaying: !state.isPlaying
-              }))
-            }
-          >
+          <button onClick={this.togglePlayback}>
             {this.state.isPlaying ? "Stop" : "Start"} audio
           </button>
-
-          <div>
-            <h2>Position</h2>
-            <p>
-              <button onClick={() => this.setState({ position: [-1, 0, 0] })}>
-                Left
-              </button>
-              <button onClick={() => this.setState({ position: [1, 0, 0] })}>
-                Right
-              </button>
-              <button onClick={() => this.setState({ position: [0, 0, 1] })}>
-                Front
-              </button>
-              <button onClick={() => this.setState({ position: [0, 0, -1] })}>
-                Back
-              </button>
-              <button onClick={() => this.setState({ position: [0, 1, 0] })}>
-                Top
-              </button>
-              <button onClick={() => this.setState({ position: [0, -1, 0] })}>
-                Bottom
-              </button>
-            </p>
-          </div>
         </header>
-
-        {this.state.isPlaying && (
-          <Scene
-            ambisonicOrder={3}
-            dimensions={dimensions}
-            materials={materials}
-          >
-            <Source src="/audio/wind.m4a" position={this.state.position} />
-          </Scene>
-        )}
       </div>
     );
   }
---
+++ src/AudioDemo.tsx
@@ -0,0 +1,82 @@
+import {
+  ResonanceAudio,
+  Source,
+  RoomDimensions,
+  RoomMaterials
+} from "resonance-audio";
+import { Vector3 } from "./react-resonance/utils";
+
+const dimensions: RoomDimensions = {
+  width: 8,
+  height: 4,
+  depth: 8
+};
+
+const materials: RoomMaterials = {
+  left: "brick-bare",
+  right: "curtain-heavy",
+  front: "marble",
+  back: "glass-thin",
+  down: "grass",
+  up: "transparent"
+};
+
+// Circular position based on timestamp
+const f = (t: number): Vector3 => [
+  3 * Math.sin((t / 4000) * 2 * Math.PI),
+  1,
+  3 * Math.cos((t / 4000) * 2 * Math.PI)
+];
+
+export default class AudioDemo {
+  audioContext: AudioContext;
+  scene: ResonanceAudio;
+  audioSource: AudioBufferSourceNode;
+  source: Source;
+
+  lastT = 0;
+  raf = 0;
+
+  constructor() {
+    this.audioContext = new AudioContext();
+    this.scene = new ResonanceAudio(this.audioContext, {
+      ambisonicOrder: 3,
+      listenerPosition: new Float32Array([0, 1, 0]),
+      dimensions,
+      materials
+    });
+    this.scene.output.connect(this.audioContext.destination);
+    this.audioSource = this.audioContext.createBufferSource();
+    this.audioSource.loop = true;
+    this.source = this.scene.createSource({
+      position: new Float32Array([0, 1, 3]),
+      forward: new Float32Array([1, 0, 0])
+    });
+    this.audioSource.connect(this.source.input);
+  }
+
+  async start(src: string) {
+    const resp = await fetch(src);
+    const data = await resp.arrayBuffer();
+    const buffer = await this.audioContext.decodeAudioData(data);
+
+    this.audioSource.buffer = buffer;
+    this.audioSource.start();
+
+    this.raf = requestAnimationFrame(this.update);
+  }
+
+  stop() {
+    cancelAnimationFrame(this.raf);
+    this.audioSource.stop();
+    this.audioSource.buffer = null;
+  }
+
+  update: FrameRequestCallback = t => {
+    const [x, y, z] = f(t);
+    this.source.setPosition(x, y, z);
+    console.log(x, y, z);
+
+    this.raf = requestAnimationFrame(this.update);
+  };
+}
