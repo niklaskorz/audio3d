f0db23d42ab129618d045611e9f20eded80d9fff
Niklas Korz <korz.niklask@gmail.com>
2019-03-26 11:11:36 +0100

    Add Safari fallback for web audio api

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,12 @@ import Editor from "./editor/Editor";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
+const global = window as any;
+// Safari does not yet support the Web Audio API without a prefix
+if (!global.AudioContext && global.webkitAudioContext) {
+  global.AudioContext = global.webkitAudioContext;
+}
+
 ReactDOM.render(<Editor />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
