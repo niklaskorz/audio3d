70fa3995bb8120addd79e917bf297eea65db7fd8
CI Build <cibuild@niklaskorz.de>
2019-04-13 09:56:07 +0000

    [skip ci] Updates

--- precache-manifest.231368165ae1380680d7f17e2164be33.js
+++ precache-manifest.231368165ae1380680d7f17e2164be33.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "40c7848dc9e09bbcb8e5",
+    "revision": "c5d1480d622b345602ac",
     "url": "/audio3d/static/css/main.fabddf17.chunk.css"
   },
   {
-    "revision": "40c7848dc9e09bbcb8e5",
-    "url": "/audio3d/static/js/main.40c7848d.chunk.js"
+    "revision": "c5d1480d622b345602ac",
+    "url": "/audio3d/static/js/main.c5d1480d.chunk.js"
   },
   {
-    "revision": "f6796ceae4fcd9c06b02",
+    "revision": "8d2eb1c25a22fed1892d",
     "url": "/audio3d/static/css/1.aa09c7ea.chunk.css"
   },
   {
-    "revision": "f6796ceae4fcd9c06b02",
-    "url": "/audio3d/static/js/1.f6796cea.chunk.js"
+    "revision": "8d2eb1c25a22fed1892d",
+    "url": "/audio3d/static/js/1.8d2eb1c2.chunk.js"
   },
   {
     "revision": "55b4293804ac874bf7a8",
     "url": "/audio3d/static/js/runtime~main.55b42938.js"
   },
   {
-    "revision": "c4806a8a3897f38cabcc8d6f8fc238ba",
+    "revision": "a67fa2b5c16964c88301a3c20492dc61",
     "url": "/audio3d/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/audio3d/precache-manifest.b1f259467343f25f22a4c0b141dcc7d0.js"
+  "/audio3d/precache-manifest.231368165ae1380680d7f17e2164be33.js"
 );
 
 workbox.clientsClaim();
