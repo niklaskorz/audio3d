070819fb9f8337ec7b62fc32991cbe7169ff0bb9
Daniel Salomon <30504863+Salomon-MH@users.noreply.github.com>
2019-04-13 16:05:39 +0200

    Collision detection, player height and other stuff for runtime

--- src/runtime/Runtime.ts
+++ src/runtime/Runtime.ts
@@ -1,7 +1,7 @@
 /**
  * @author Daniel Salomon
  */
-import { Raycaster, Vector3, WebGLRenderer } from "three";
+import { Raycaster, Vector3, WebGLRenderer, Object3D, Mesh } from "three";
 import GamepadListener from "../input/GamepadListener";
 import KeyboardListener from "../input/KeyboardListener";
 import Project from "../project/Project";
@@ -18,11 +18,14 @@ export default class Runtime {
 
   rafHandle = 0;
   previousTimestamp = 0;
+  playerHeight = 1.8; //1,80m player height, eyes are ~10cm lower
 
   renderer = new WebGLRenderer();
   canvas: HTMLCanvasElement;
 
   raycaster = new Raycaster();
+
+  dummyCamera = new Object3D();
 
   keys = new KeyboardListener(this.renderer.domElement);
   gamepads = new GamepadListener();
@@ -33,7 +36,7 @@ export default class Runtime {
     this.canvas.tabIndex = -1; // Make element focusable
 
     this.project.selectRoom(this.project.rooms[0]);
-    this.project.camera.position.set(0, 1, 2);
+    this.project.camera.position.set(0, this.playerHeight - 0.1, 2);
     this.project.camera.rotation.set(0, 0, 0);
   }
 
@@ -96,29 +99,45 @@ export default class Runtime {
     this.renderer.render(this.project.activeRoom, this.project.camera);
   };
 
+  isBetween(check: number, from: number, to: number): boolean {
+    let min = Math.min.apply(Math, [from, to]),
+      max = Math.max.apply(Math, [from, to]);
+    return check >= min && check <= max;
+  }
+
   update(dt: number): void {
     const { camera } = this.project;
+    this.dummyCamera.position.copy(camera.position);
+    this.dummyCamera.rotation.copy(camera.rotation);
+
     let moveX = 0;
     let moveZ = 0;
+    let rotateY = 0;
 
     if (this.keys.isPressed("w")) {
-      camera.translateOnAxis(axes.z, -2 * dt);
+      let newZ = -2 * dt;
+      moveZ = Math.abs(moveZ) < Math.abs(newZ) ? newZ : moveZ;
     }
     if (this.keys.isPressed("s")) {
-      camera.translateOnAxis(axes.z, 2 * dt);
+      let newZ = 2 * dt;
+      moveZ = Math.abs(moveZ) < Math.abs(newZ) ? newZ : moveZ;
     }
     if (this.keys.isPressed("a")) {
-      camera.translateOnAxis(axes.x, -2 * dt);
+      let newX = -2 * dt;
+      moveX = Math.abs(moveX) < Math.abs(newX) ? newX : moveX;
     }
     if (this.keys.isPressed("d")) {
-      camera.translateOnAxis(axes.x, 2 * dt);
+      let newX = 2 * dt;
+      moveX = Math.abs(moveX) < Math.abs(newX) ? newX : moveX;
     }
 
     if (this.keys.isPressed("ArrowLeft")) {
-      camera.rotateOnWorldAxis(axes.y, dt);
+      let newY = dt;
+      rotateY = Math.abs(rotateY) < Math.abs(newY) ? newY : rotateY;
     }
     if (this.keys.isPressed("ArrowRight")) {
-      camera.rotateOnWorldAxis(axes.y, -dt);
+      let newY = -dt;
+      rotateY = Math.abs(rotateY) < Math.abs(newY) ? newY : rotateY;
     }
 
     const gamepadAxes = {
@@ -128,13 +147,57 @@ export default class Runtime {
     };
     // console.log(axes);
     if (gamepadAxes.x) {
-      camera.translateOnAxis(axes.x, 2 * dt * gamepadAxes.x);
+      let newX = 2 * dt * gamepadAxes.x;
+      moveX = Math.abs(moveX) < Math.abs(newX) ? newX : moveX;
     }
     if (gamepadAxes.y) {
-      camera.translateOnAxis(axes.z, 2 * dt * gamepadAxes.y);
+      let newZ = 2 * dt * gamepadAxes.y;
+      moveZ = Math.abs(moveZ) < Math.abs(newZ) ? newZ : moveZ;
     }
     if (gamepadAxes.rX) {
-      camera.rotateOnWorldAxis(axes.y, -dt * gamepadAxes.rX);
+      let newY = -dt * gamepadAxes.rX;
+      rotateY = Math.abs(rotateY) < Math.abs(newY) ? newY : rotateY;
+    }
+
+    // Executing the rotation/movement after all keys are evaluated prevents the issue of double movement speed when both, keyboard and gamepad, is used at the same time.
+    camera.rotateOnWorldAxis(axes.y, rotateY); // Rotate left & right
+    //Apply movement to dummy object first to check if a collision is happening. If not, apply to actual camera.
+    this.dummyCamera.translateOnAxis(axes.z, moveZ); // Move forwards & backwards
+    this.dummyCamera.translateOnAxis(axes.x, moveX); // Move left & right
+
+    //Values to check for collision
+    let newX = this.dummyCamera.position.x;
+    let newZ = this.dummyCamera.position.z;
+
+    //Check for collision
+    let collided = false;
+    for (let i of this.project.activeRoom.children) {
+      if (!(i instanceof Mesh)) {
+        continue;
+      }
+      const thresholdMovement = 0.25; //Minimum allowed distance to an object
+      const thresholdHeight = 0.05; //Minimum allowed distance to an object below or above the player (player height is 1,80[m])
+      let boundX1 = i.position.x + i.scale.x / 2 + thresholdMovement;
+      let boundX2 = i.position.x - i.scale.x / 2 - thresholdMovement;
+      let boundY1 = i.position.y + i.scale.y / 2 + thresholdHeight;
+      let boundY2 = i.position.y - i.scale.y / 2 - thresholdHeight;
+      let boundZ1 = i.position.z + i.scale.z / 2 + thresholdMovement;
+      let boundZ2 = i.position.z - i.scale.z / 2 - thresholdMovement;
+
+      if (
+        this.isBetween(newX, boundX2, boundX1) &&
+        this.isBetween(newZ, boundZ2, boundZ1) &&
+        (this.isBetween(boundY1, 0, this.playerHeight) ||
+          this.isBetween(boundY2, 0, this.playerHeight) ||
+          (this.isBetween(this.playerHeight, boundY2, boundY1) &&
+            this.isBetween(0, boundY2, boundY1)))
+      ) {
+        collided = true;
+      }
+    }
+
+    if (!collided) {
+      camera.position.copy(this.dummyCamera.position);
     }
   }
 }
