fde0ac42f78a65d3129e65dc6f4305faa387e689
Niklas Korz <korz.niklask@gmail.com>
2019-04-14 16:12:03 +0200

    Implement code block editor

---
+++ src/project/CodeBlock.ts
@@ -0,0 +1,20 @@
+export default class CodeBlock {
+  source: string;
+  func?: Function;
+
+  constructor(source: string = "") {
+    this.source = source;
+  }
+
+  update(source: string): void {
+    this.source = source;
+    this.func = undefined;
+  }
+
+  execute(playerState: any, roomState: any, thisObject: any = null): void {
+    if (!this.func) {
+      this.func = new Function("playerState", "roomState", this.source);
+    }
+    this.func.call(thisObject, playerState, roomState);
+  }
+}
