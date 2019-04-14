9d4f4348c379dddfc095cda36f36df2c30bf0e23
Niklas Korz <korz.niklask@gmail.com>
2019-04-14 16:40:31 +0200

    Add close page prompt

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,16 @@ if (!global.AudioContext && global.webkitAudioContext) {
   global.AudioContext = global.webkitAudioContext;
 }
 
+if (process.env.NODE_ENV === "production") {
+  // If we are running in the production environment, make sure the user
+  // does not close the editor unintentionally.
+  window.addEventListener(
+    "beforeunload",
+    () =>
+      "Do you really want to close the editor? You might have unsaved changes."
+  );
+}
+
 ReactDOM.render(<Editor />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
